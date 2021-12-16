86034eb9f74e0878c918861ccf4051f64685ba2d
pa <pramodh.ayyappan92@gmail.com>
2021-12-16 22:08:24 +0530

    added slash to end of source and target dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,4 +7,4 @@ target_dir = "{{ cookiecutter.destination_dir }}"
 if( not target_dir.startswith('#')):
     files_list = os.listdir(source_dir)
     for file in files_list:
-        shutil.move(source_dir + file, target_dir + file)
+        shutil.move(source_dir + "/" + file, target_dir + "/" + file)
